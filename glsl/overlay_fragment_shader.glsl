20d94a735a6a44b9f51dc09398679885666c09d7
Asurahan <logic@xaker.ru>
2025-02-05 12:56:56 +0400

    cleanig projekt

--- glsl/overlay_fragment_shader.glsl
+++
@@ -1,9 +0,0 @@
-#version 330 core
-out vec4 FragColor;
-
-uniform vec4 overlayColor; // Добавляем uniform переменную для цвета
-
-void main()
-{
-    FragColor = overlayColor; // Используем переданный цвет
-}
